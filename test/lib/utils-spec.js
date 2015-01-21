7061a9c68ba80bd676411af5fde66080c5bb3ca9
Will Bamford <w.bamford@gmail.com>
2015-01-21 07:31:12 +0000

    Added a test for 'transformHeaders' utils method.

--- test/lib/utils-spec.js
+++ test/lib/utils-spec.js
@@ -4,6 +4,16 @@ var utils = require('../../src/lib/utils.js');
 
 describe('utils', function () {
 
+  describe('transformHeaders', function () {
+    it('should transform a header string into a dictionary', function () {
+      expect(utils.transformHeaders('Header-1: Value-1\r\n Header-2: Value:A:B:C'))
+        .toEqual({
+              'Header-1': 'Value-1',
+              'Header-2': 'Value:A:B:C'
+        });
+    });
+  });
+
   describe('addQueryStringParamToUrl', function () {
 
     it('should add a query string separator (?) before key-value', function () {
